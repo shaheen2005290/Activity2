0b9ddaded194f165f8d84096d92f252a99067617
shaheen2005290 <67104103+shaheen2005290@users.noreply.github.com>
2020-06-19 13:38:22 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int reverse(int);
+int main()
+{
+    int n,rev;
+    printf("Enter a number:\n");
+    scanf("%d",&n);
+    rev=reverse(n);
+    printf("%d\n",rev)
+    if(rev==n);
+        printf("The given number %d is a palindrome\n",n);
+    else
+        printf("The given number %d is not a palindrome\n");
+    return 0;
+}
+int reverse(int n)
+{
+    int val,base=1,real=0;
+    while(n!=0)
+    {
+        val=n%10;
+        real=real*base*10+val;
+        n=n/10;
+    }
+
+    return real;
+}
